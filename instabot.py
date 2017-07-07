c8a797ca934780308dfd5aedbc63b8704e7f9c31
surbhi rawat <surbhirawat96@gmail.com>
2017-07-08 00:00:06 +0530

    likes and comments added

--- instabot.py
+++ instabot.py
@@ -69,9 +69,11 @@ def get_user_info(insta_username):
      if user_info['meta']['code']== 200:
          if len(user_info['data']):
              print 'username: %s' % (user_info['data']['username'])
+             print 'fullname: %s' % (user_info['data']['full_name'])
              print 'no. of followers: %s' % (user_info['data']['counts']['followed_by'])
              print 'no. of people following: %s' % (user_info['data']['counts']['follows'])
              print 'no. of posts: %s' % (user_info['data']['counts']['media'])
+             print 'BIO: %s' % (user_info['data']['bio'])
          else:
              print 'there is no data available for this user'
 
@@ -124,6 +126,63 @@ def get_users_post(insta_username):
         print 'status code other than 200 received'
 
 
+#declare a function to get the Id of the recent post of the user
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'user does not exist!'
+        exit()
+    request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, my_token)
+    print 'GET request url: %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    #check the status code
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print 'ther is no recent post of the user'
+
+    else:
+        print 'status code other than 200 received'
+
+        exit()
+
+#defining function to like the post of the user
+#access token is sent in payload to make post request (to like the post)
+def like_post(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url = (base_url + 'media/%s/likes') % (media_id)
+    payload ={"access_token": my_token}
+    print 'POST request url: %s' % (request_url)
+    post_a_like = requests.post(request_url,payload).json()
+
+    #to check if like was successful by checking the status code
+    if post_a_like['meta']['code'] == 200:
+        print 'the like was successful'
+
+    else:
+        print 'your like was unsuccessful. try again!'
+
+#defining function to post a comment on the recent post of the user
+def post_a_comment(insta_username):
+    # call the function get_post_id to get the id of the post in which we need to make a comment
+    media_id =get_post_id(insta_username)
+    #use the media-id to make a post request. payload will consists of the access token and the comment to be added
+    comment_text = raw_input("your comment: ")
+    payload = {"access_token": my_token,"text": comment_text}
+    request_url = (base_url + 'media/%s/comments') % (media_id)
+    print 'POST request url: %s'% (request_url)
+
+    make_comment = requests.post(request_url,payload).json()
+
+    #to check if comment was successful or not, while checking the status code
+    if make_comment['meta']['code'] == 200:
+        print 'your new comment is added successfully!'
+    else:
+        print 'unable to add the comment. Try Again!'
+
+
  #options available -what you want to do with the access token
 def start_bot():
      while True:
@@ -134,6 +193,9 @@ def start_bot():
          print "b.get details of a user by username\n"
          print "c.get your own recent post\n"
          print "d.get the recent post of the user by username\n"
+         print "e.like the recent post of the user\n"
+         print "f.make a comment on the recent post of the user\n"
+
          print 'x.EXIT'
 
          choice = raw_input("enter your choice: ")
@@ -147,6 +209,13 @@ def start_bot():
          elif choice == "d":
              insta_username = raw_input("enter the username of the user: ")
              get_users_post(insta_username)
+         elif choice == "e":
+             insta_username = raw_input("enter the username of the user:")
+             like_post(insta_username)
+         elif choice == "f":
+             insta_username = raw_input("enter the username of the user:")
+             post_a_comment(insta_username)
+
 
          elif choice == "x":
              exit()
